2de90496438dbf5fa3b17dab9442861777cbd23c
dabhade904 <dabhade904@gmail.com>
2020-03-17 14:19:29 +0530

    Added : regex pattern for validate admin information

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react'
 import AdminLogin from '../src/component/adminComponent/singUpAndSign'
+// import Footer from './component/footer/footer'
+// import Validation from './component/validation/validation'
 export class App extends Component {
   render() {
     return (
       <div>
         <AdminLogin/>
+      {/* <Footer/> */}
+      {/* <Validation/> */}
       </div>
     )
   }
